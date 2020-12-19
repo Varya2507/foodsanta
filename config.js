8de3509df21ab067d00f1e085800297dfac879e8
Varya2507 <63450901+Varya2507@users.noreply.github.com>
2020-12-19 17:42:00 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+import firebase from "firebase";
+require('@firebase/firestore')
+
+
+var firebaseConfig = {
+    apiKey: "AIzaSyBnkKoKs8rxk3XwY9Z97RdtTdwXvZ9fRnY",
+    authDomain: "food-santa-b7f59.firebaseapp.com",
+    projectId: "food-santa-b7f59",
+    storageBucket: "food-santa-b7f59.appspot.com",
+    messagingSenderId: "1037911891116",
+    appId: "1:1037911891116:web:d5e0e6e1d14b40dfa21309"
+  };
+
+  // Initialize Firebase
+  if(!firebase.apps.length){ firebase.initializeApp(firebaseConfig); }
+export default firebase.firestore();
